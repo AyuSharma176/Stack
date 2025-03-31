2e5bccfd551030348f8779762c56df8f68bf2dfb
Ayush Sharma <ayushoffinfo17@gmail.com>
2025-04-01 00:41:41 +0530

    Stack Implementation

--- src/Stack_Ques/Infix.java
+++ src/Stack_Ques/Infix.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Infix {
     public static void main(String...args){
-        String s = "9-5+3*4/6";
+        String s = "9-(5+3)*4/6";
         System.out.println(s);
         Stack<Integer> val = new Stack<>();
         Stack<Character> op = new Stack<>();
@@ -15,7 +15,19 @@ public class Infix {
             if(ascii >= 48 && ascii <= 57){
                 val.push(ascii-48);
             }
-            else if(op.isEmpty()) op.push(ch);
+            else if(op.isEmpty() || ch=='(' || op.peek()=='(') op.push(ch);
+            else if(ch==')'){
+                while(op.peek()!='('){
+                    int v2 = val.pop();
+                    int v1 = val.pop();
+                    if(op.peek() == '-') val.push(v1-v2);
+                    if(op.peek() == '+') val.push(v1+v2);
+                    if(op.peek() == '*') val.push(v1*v2);
+                    if(op.peek() == '/') val.push(v1/v2);
+                    op.pop();
+                }
+                op.pop();
+            }
             else{
                 if(ch == '-' || ch == '+'){
                     int v2 = val.pop();
